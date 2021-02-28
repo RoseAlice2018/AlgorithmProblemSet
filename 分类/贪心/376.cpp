b535f9df7775964a148672d353783125657e5416
RoseAlice2018 <1505243015@qq.com>
2021-02-28 21:06:03 +0800

    update 2.28 贪心

---
+++ 分类/贪心/376.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        if (nums.size() <= 1) return nums.size();
+        int curDiff = 0; // 当前一对差值
+        int preDiff = 0; // 前一对差值
+        int result = 1;  // 记录峰值个数，序列默认序列最右边有一个峰值
+        for (int i = 1; i < nums.size(); i++) {
+            curDiff = nums[i] - nums[i - 1];
+            // 出现峰值
+            if ((curDiff > 0 && preDiff <= 0) || (preDiff >= 0 && curDiff < 0)) {
+                result++;
+                preDiff = curDiff;
+            }
+        }
+        return result;
+    }
+};
